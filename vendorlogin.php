d3cb5a03b3ac28c843fd38a1e3a560ff98e4d454
Cottonery <micheldreemur89@gmail.com>
2024-04-06 19:59:33 +0700

    finished login page, just needed a background image

--- vendorlogin.php
+++ vendorlogin.php
@@ -1,33 +1,32 @@
 <?php
-// Include config file
-require_once 'config.php';
+    require_once 'config.php';
 
-if (isset($_POST['submit'])) {
+    if (isset($_POST['submit'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
 
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    //The sql statement
-    $sql = "select * from store where store_name = ? and store_password = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ss", $username, $password);    
-    $stmt->execute();
-    $result = $stmt->get_result(); 
-    $result = $stmt->get_result();
+        $sql = "SELECT store_id FROM store WHERE store_brand = ? AND store_password = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("ss", $username, $password);    
+        $stmt->execute();
+        $result = $stmt->get_result(); 
 
-    if ($result->num_rows > 0) {
-        session_start();
-        $_SESSION['username'] = $username;
-        header("Location: vendorhome.html");
-        exit;
+        if ($result->num_rows > 0) {
+            $id = $result->fetch_assoc();
+            $store_id = $id['store_id'];
+
+            session_start();
+            $_SESSION['store_id'] = $store_id;
+            header("Location: vendorhome.html");
+            exit;
+        } else {
+            header("Location: vendorlogin.html?error=Invalid username or password");
+            exit;
+        }
+        $stmt->close();
+        $conn->close();
+
     } else {
-        echo "Invalid username or password";
+        header("Location: vendorlogin.html?error=What are you doing?");
     }
-
-    $stmt->close();
-    $conn->close();
-
-} else {
-
-}
-//Run a test, fix the appliance of error message
 ?>
