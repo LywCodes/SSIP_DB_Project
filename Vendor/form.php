a08f76bc21741d2bea598496e10905727141b143
Clania Elmymora <130958652+Clemoryn@users.noreply.github.com>
2024-04-15 17:09:48 +0700

    foto udh masuk

--- Vendor/form.php
+++ Vendor/form.php
@@ -1,36 +1,62 @@
 <?php
-  $connect = mysqli_connect("localhost", "root", "", "e_canteen");
-  if ($connect->connect_error) {
-      die("Connection failed: " . $connect->connect_error);
-    }
+
+ $connect = mysqli_connect("localhost", "root", "", "e_canteen");
+ if (mysqli_connect_error()) {
+     die("Connection failed: " . mysqli_connect_error());
+ }
 ?>
 <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
-        $product_name = $_POST['product_name'];
-        $price = $_POST['price'];
-        $description = $_POST['description'];
-        $availability = $_POST['availability'];
+//ini untuk submit button
+   if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
+       $product_name = $_POST['product_name'];
+       $price = $_POST['price'];
+       $description = $_POST['description'];
+       $availability = $_POST['availability'];
 
-        $connect = mysqli_connect("localhost", "root", "", "e_canteen");
-        if ($connect->connect_error) {
-            die("Connection failed: " . $connect->connect_error);
-        }
+       $connect = mysqli_connect("localhost", "root", "", "e_canteen");
+       if (mysqli_connect_error()) {
+           die("Connection failed: " . mysqli_connect_error());
+       }
+//ini untuk foto2 product 
+       $target_pict = "image/";
+       $fileNames = basename($_FILES["image"]["name"]);
+       $target_file = $target_pict . $fileNames;
+       $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+       $image_size = $_FILES["image"]["size"];
+       
+       if (!isset($_FILES["image"]["tmp_name"])) {
+           echo "File is not uploaded.";
+           exit();
+       }
+       if ($image_size > 500000) {
+           echo "Sorry, your file is too large.";
+           exit();
+       }
+       if (!in_array($imageFileType, ["jpg", "jpeg", "png"])) {
+           echo "Sorry, only JPG, JPEG & PNG files are allowed.";
+           exit();
+       }
+       if (!move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+           echo "Sorry, there was an error uploading your file.";
+           exit();
+       }
+//ini untuk menggabungkan data2
+       $saveQuery = "INSERT INTO product (product_name, product_price, product_description, product_availability, product_image)
+           VALUES ('$product_name', '$price', '$description', '$availability', '$fileNames')";
 
-        $saveQuery = "INSERT INTO product (product_name, product_price, product_description, product_availability)
-            VALUES ('$product_name', '$price', '$description', '$availability')";
+       if (mysqli_query($connect, $saveQuery)) {
+           echo "berhasil hehe";
+           header("Location: " . $_SERVER['PHP_SELF']);
+     exit();
+       } else {
+           echo "Error: ";
+       }
+       mysqli_close($connect);
+     }
+     ?>
 
-        if ($connect->query($saveQuery) === TRUE) {
-            echo "berhasil hehe";
-            header("Location: " . $_SERVER['PHP_SELF']);
-      exit();
-        } else {
-            echo "Error: ";
-        }
-      
-        $connect->close();
-      }
-      ?>
 <?php
+//ini untuk delete
   if (isset($_GET['delete_id'])) {
       $delete_id = $_GET['delete_id'];
       $delete_query = "DELETE FROM product WHERE product_id = $delete_id";
@@ -42,9 +68,9 @@
       }
   }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
  <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -53,7 +79,7 @@
     <style>
         body {
             font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-            background-image: url('./image/wallpaper.jpg');
+            background-image: url('img/wallpaper.jpg');
             
         }
 
@@ -71,7 +97,7 @@
             border-radius: 10px;
             padding: 30px;
             box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
-            color:black;
+            text-color:black;
         }
 
         .form-control {
@@ -98,58 +124,56 @@
         }
 
         table {
-            width: 100%;
-            color: #343a40; /* Warna teks utama */
-            border-collapse: collapse;
-            border-radius: 10px;
-            overflow: hidden;
-            box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
-            background-color: #ffffff;
-        }
+    width: 100%;
+    color: #343a40; /* Warna teks utama */
+    border-collapse: collapse;
+    border-radius: 10px;
+    overflow: hidden;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+    background-color: #ffffff;
+}
 
-        th, td {
-            padding: 15px;
-            text-align: left;
-            border-bottom: 1px solid #dee2e6;
-            color: #343a40; /* Warna teks di dalam sel */
-        }
+th, td {
+    padding: 15px;
+    text-align: left;
+    border-bottom: 1px solid #dee2e6;
+    color: #343a40; /* Warna teks di dalam sel */
+}
 
-        th {
-            background-color: #343a40;
-            color: #ffffff;
-            text-transform: uppercase;
-            letter-spacing: 1px;
-        }
+th {
+    background-color: #343a40;
+    color: #ffffff;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+}
 
         tr:nth-child(even) {
-            background-color: #f8f9fa; 
-        
+            background-color: #f8f9fa;
         }
 
         .delete-link {
-            color: #fff;
-            background-color: #dc3545;
-            border: none;
-            padding: 8px 12px;
-            border-radius: 5px;
-            text-decoration: none;
-            transition: background-color 0.3s;
-            display: inline-block;
-        }
+        color: #fff;
+        background-color: #dc3545;
+        border: none;
+        padding: 8px 12px;
+        border-radius: 5px;
+        text-decoration: none;
+        transition: background-color 0.3s;
+        display: inline-block;
+    }
 
-        .delete-link:hover {
-            background-color: orange;
-        }
-</style>
+    .delete-link:hover {
+        background-color: orange;
+    }
+    </style>
 </head>
 <body>
 <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">>
-            <a class="navbar-brand" href="#"><img src="./source/logo.png" alt="Logo" style="position: absolute; top: 20px; left: 20px; width: 120px; height: auto;"</a>
+            <a class="navbar-brand" href="#"><img src="./img/logo.png" alt="Logo" style="position: absolute; top: 20px; left: 20px; width: 120px; height: auto;"</a>
             <button class="navbar-toggler" type ="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
@@ -172,7 +196,7 @@
         <h2>Product Input Form</h2>
         <div class="row justify-content-center">
             <div class="col-md-6 form-container">
-                <form action="" method="post">
+                <form action="" method="post" enctype="multipart/form-data">
                     <div class="form-group">
                         <label for="product_name">Product Name:</label>
                         <input type="text" class="form-control" id="product_name" name="product_name" required>
@@ -221,8 +245,11 @@
 
   if ($result->num_rows > 0) {
 ?>
-  <table>
-    <?php while ($row = $result->fetch_assoc()) { ?>
+  <table> 
+    <?php 
+    //ini table bersih dan rapih
+        while ($row = $result->fetch_assoc()) {  
+    ?>
       <tr>
         <td><?php echo $row['product_name']; ?></td>
         <td><?php echo $row['product_price']; ?></td>
@@ -230,9 +257,13 @@
         <td><?php echo $row['product_description']; ?></td>
         <td><?php echo $row['product_availability']; ?></td>
       </tr>
-    <?php } ?>
+    <?php 
+        } 
+    ?>
   </table>
-<?php } ?>
+<?php 
+    }
+?>
 
 <?php
   $connect->close();
@@ -240,7 +271,7 @@
 
 <br>
     <div class="table-container">
-            <h2>Daftar Produk</h2>
+            <h2>Product List</h2>
             <table class="table table-bordered">
                 <thead>
                     <tr>
@@ -265,10 +296,11 @@
             if ($result->num_rows > 0) {
                 while ($row = $result->fetch_assoc()) {
                     ?>
+                    <!--ini table kotor (untuk edit2)-->
                     <tr>
                         <td><?php echo $row['product_name']; ?></td>
                         <td><?php echo $row['product_price']; ?></td>
-                        <td><?php echo $row['product_image']; ?></td> 
+                        <td><img src="img/<?php echo $row['product_image']; ?>" alt="Product Image" width="200px"; heigth="200px"; ></td> 
                         <td><?php echo $row['product_description']; ?></td>
                         <td><?php echo $row['product_availability']; ?></td>
                         <td>
